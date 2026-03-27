1d465c0d75111444b45ffc4cd1ecd089269540e9
guangtoutong <lixd220@gmail.com>
2026-03-28 00:19:01 +0800

    fix: 修复 TypeScript 类型定义

--- env.d.ts
+++ env.d.ts
@@ -1,14 +1,12 @@
 /// <reference types="@cloudflare/workers-types" />
 
-interface CloudflareEnv {
-  DB: D1Database;
-  ODDS_API_KEY?: string;
+declare module "@cloudflare/next-on-pages" {
+  export function getRequestContext(): {
+    env: {
+      DB: D1Database;
+      ODDS_API_KEY?: string;
+    };
+    ctx: ExecutionContext;
+    cf: IncomingRequestCfProperties;
+  };
 }
-
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv extends CloudflareEnv {}
-  }
-}
-
-export {};
